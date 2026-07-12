91a122b3507d2e8872c682210edd25aa5f858da2
Raj Mohan Acharya <rajmohanacharya@protonmail.com>
2026-07-12 19:38:30 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.RAJAI_CONFIG = Object.freeze({
   endpointManifestApiUrl: "https://api.github.com/repos/rajmohanacharya/ai/contents/runtime-endpoint.json?ref=main",
   endpointManifestFallbackUrl: "./runtime-endpoint.json",
-  offlineMessage: "Notify Raj to start the Local AI in Colab.",
   notifyUrl: "",
   statusPollMs: 30000,
   manifestPollMs: 120000
